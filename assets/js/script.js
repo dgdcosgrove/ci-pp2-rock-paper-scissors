1fd06db90eb0f72cf4825a8cb843cafff09d783e
dgdcosgrove <dgdcosgrove@gmail.com>
2022-10-17 21:38:54 +0000

    Add event listeners for DOM loading and choice clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,24 @@
+// Wait for DOM content to load before running the game
+// Get clickable images "buttons" and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByClassName("control-img");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "rock") {
+                alert("You clicked Rock!");
+            } else if (this.getAttribute("data-type") === "paper") {
+                alert("You clicked Paper!");
+            } else if (this.getAttribute("data-type") === "scissors") {
+                alert("You clicked Scissors!");
+            } else {
+                alert("You didn't make a choice");
+            }
+        })
+    }
+})
+
 function runGame() {
 
 }
@@ -23,6 +44,5 @@ function resultMessage() {
 }
 
 function resultExhibit() {
-    
-}
 
+}
